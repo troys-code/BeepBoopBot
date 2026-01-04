a7409adb64a0fe20dbfa66384a1883315dc5aefd
Troy <troy-lamerton@users.noreply.github.com>
2026-01-04 18:21:01 +1300

    fix: remove webpack references and vite debugging config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({gzipSize: true}),
   ],
   build: {
     outDir: 'webpage',
